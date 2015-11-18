40e17447ba2fc9b90ef99c68cbd58ebc871da82d
potoczny <natalia.potoczny@gmail.com>
2015-11-18 22:43:44 +0100

    Simulation added

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -2,6 +2,7 @@ import genetic.functions.Crossers.{BasicListCrosser, BasicNetworkCrosser}
 import genetic.functions.Functions
 import genetic.functions.Mutators.{BasicListMutator, BasicNetworkMutator}
 import genetic.functions.Strategies.BasicStrategy
+import genetic.functions.Simulation
 import individual.list.ListToSort
 import individual.network.SortingNetwork
 
@@ -18,28 +19,20 @@ object Main {
     val listLength = 5
     val listsPopulationSize = 40
     val networksPopulationSize = 40
-    val startingNetworkSize = 5
-
-    val startingLists = (1 to listsPopulationSize).map(x => ListToSort.randomList(listLength)).toArray
-    val startingNetworks = (1 to networksPopulationSize).map(x => SortingNetwork.randomNetwork(listLength, startingNetworkSize)).toArray
-
-    var populations = (startingNetworks, startingLists)
 
     val strategy = new BasicStrategy(listsPopulationSize, networksPopulationSize, listLength, BasicNetworkMutator,
       BasicNetworkCrosser, BasicListMutator, BasicListCrosser, Functions.networkTarget, Functions.listTarget)
 
-    for (x <- 1 to 100) {
-      populations = strategy.evolve(populations)
-    }
+    val (startingLists, startingNetworks, finalLists, finalNetworks) = Simulation.simulate(listLength,listsPopulationSize,networksPopulationSize,5,100,strategy)
 
     println("Starting lists, Starting networks")
     printFunctions(startingLists, startingNetworks)
     println("Starting lists, Final networks")
-    printFunctions(startingLists, populations._1)
+    printFunctions(startingLists,finalLists)
     println("Final lists, Starting networks")
-    printFunctions(populations._2, startingNetworks)
+    printFunctions(finalNetworks, startingNetworks)
     println("Final lists, Final networks")
-    printFunctions(populations._2, populations._1)
+    printFunctions(finalNetworks, finalLists)
   }
 
   def printFunctions(lists: Array[ListToSort], networks: Array[SortingNetwork]) = {
---
+++ src/main/scala-2.11/genetic/functions/Simulation.scala
@@ -0,0 +1,22 @@
+package genetic.functions
+
+import genetic.Strategy
+import individual.list.ListToSort
+import individual.network.SortingNetwork
+
+object Simulation {
+
+  def simulate(listLength: Int, listsPopulationSize: Int, networksPopulationSize: Int, startingNetworkSize: Int, numberOfSteps: Int, strategy: Strategy[SortingNetwork,ListToSort]) = {
+    val startingLists = (1 to listsPopulationSize).map(x => ListToSort.randomList(listLength)).toArray
+    val startingNetworks = (1 to networksPopulationSize).map(x => SortingNetwork.randomNetwork(listLength, startingNetworkSize)).toArray
+
+    var populations = (startingNetworks, startingLists)
+
+    for (x <- 1 to numberOfSteps) {
+      populations = strategy.evolve(populations)
+    }
+
+    (startingLists, startingNetworks, populations._1, populations._2)
+  }
+
+}
